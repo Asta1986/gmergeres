c6d90e49b2e67cdd079c857258d09f21ea9743f3
Astaroth <asta666@xfiles.localdomain>
2017-03-19 12:28:53 -0300

    improved code readability

---
+++ AuxF.java
@@ -0,0 +1,108 @@
+package gmergeRes;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class AuxF {
+	
+	public boolean noDirSelected(File f)
+	{
+		return f == null;
+	}
+	
+	public boolean dirsSelected(File f0, File f1)
+	{
+		return (!noDirSelected(f0)) && (!noDirSelected(f1));
+	}
+	
+	public boolean supportedFile(String s)
+	{
+		return s.contains(".dat") || s.contains(".cd.res");
+	}
+	
+	public boolean existSuppFile(File[] fl)
+	{
+		boolean res = false;
+		for(File f : fl)
+		{
+			if (supportedFile(f.getName()))
+			{
+				res = true;
+				break;
+			}
+		}
+		return res;
+	}
+	
+	public void showMessage(String msg)
+	{
+		JFrame f = new JFrame();
+		JOptionPane.showMessageDialog(f, msg);
+	}
+	
+	public void proceedIfSupportedFilesInDir(File[] directoryListing, String destDir) {
+		if(existSuppFile(directoryListing))
+		{
+			PrintStream prinFile = null;
+			try 
+			{
+				prinFile = new PrintStream(destDir + File.separator + "combinedResults.txt");
+			}	 
+			catch (FileNotFoundException e1) 
+			{
+				e1.printStackTrace();
+			}					
+			for (int i = 0; i < directoryListing.length; i++) 
+			{
+				String file = directoryListing[i].getPath(), q1 = "";
+				processFileIfSupported(prinFile, file, q1);
+			}
+			prinFile.close();
+			showMessage("El proceso ha finalizado exitosamente.");		 
+		}
+		else
+	    {
+		  showMessage("El directorio especificado está vacío o no contiene\narchivos de tipo soportado (.dat y/o .cd.res.).");
+		}		 
+		
+	}
+	
+	public void processFileIfSupported(PrintStream prinFile, String file, String q1) {
+		if (supportedFile(file)) {	
+			extractTargetLines(prinFile, file, q1);
+		}
+	}
+	
+	public void extractTargetLines(PrintStream prinFile, String file, String q1) {
+		Scanner scanFile = null;
+		try 
+		{
+			scanFile = new Scanner(new File(file));
+		} 
+		catch (FileNotFoundException e1) 
+		{
+			e1.printStackTrace();
+		}
+		while (scanFile.hasNextLine())
+		{
+			if (q1.startsWith("Answer.1"))
+			{
+				prinFile.println(q1);
+				break;
+			}
+			q1 = scanFile.nextLine();
+		}
+		while(scanFile.hasNextLine())
+		{	
+			prinFile.println(scanFile.nextLine()); 
+		}
+		prinFile.println(""); 
+		scanFile.close();	
+	}
+
+}
--- GMergeJWRes.java
+++ GMergeJWRes.java
@@ -8,6 +8,9 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
@@ -39,43 +42,12 @@ public class GMergeJWRes extends JFrame
 		});
 	}
 
-	public boolean noDirSelected(File f)
-	{
-		return f == null;
-	}
-	
-	public boolean dirsSelected(File f0, File f1)
-	{
-		return (!noDirSelected(f0)) && (!noDirSelected(f1));
-	}
-	
-	public boolean supportedFile(String s)
-	{
-		return s.contains(".dat") || s.contains(".cd.res");
-	}
-	
-	public boolean existSuppFile(File[] fl)
-	{
-		boolean res = false;
-		for(File f : fl)
-		{
-			if (supportedFile(f.getName()))
-			{
-				res = true;
-				break;
-			}
-		}
-		return res;
-	}
-	
-	public void showMessage(String msg)
-	{
-		JFrame f = new JFrame();
-		JOptionPane.showMessageDialog(f, msg);
-	}
+
 		
 public GMergeJWRes() 
 {
+	AuxF auxf = new AuxF();
+	
 	setResizable(false);
 	final JFileChooser fcORG = new JFileChooser();
 	final JFileChooser fcDEST = new JFileChooser();
@@ -86,9 +58,9 @@ public GMergeJWRes()
 	
 	JButton btnNewButton = new JButton("Elegir directorio origen");
 	btnNewButton.setToolTipText("Seleccione la carpeta que contiene los archivos a procesar.");
-	btnNewButton.addMouseListener(new MouseAdapter() 
+	btnNewButton.addActionListener(new ActionListener()
 	{	@Override
-		public void mouseClicked(MouseEvent e) 
+		public void actionPerformed(ActionEvent e) 
 		{				  
 			  fcORG.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 			  fcORG.showOpenDialog(fcORG.getParent());				  
@@ -98,9 +70,9 @@ public GMergeJWRes()
 	
 	JButton btnNewButton_1 = new JButton("Elegir directorio destino");
 	btnNewButton_1.setToolTipText("Seleccione la carpeta en la que se guardará el archivo resultado.");
-	btnNewButton_1.addMouseListener(new MouseAdapter() 
+	btnNewButton_1.addActionListener(new ActionListener()
 	{	@Override
-		public void mouseClicked(MouseEvent e) 
+		public void actionPerformed(ActionEvent e) 
 		{
 			fcDEST.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 			fcDEST.showOpenDialog(fcDEST.getParent());				  		
@@ -111,96 +83,53 @@ public GMergeJWRes()
 	JButton btnCombinarArchivos = new JButton("Combinar archivos");
 	btnCombinarArchivos.setToolTipText("Copiar las partes de todos los archivos .dat y .cd.res del directorio origen a uno solo.");
 	GroupLayout groupLayout = new GroupLayout(getContentPane());
-	groupLayout.setHorizontalGroup(
-		groupLayout.createParallelGroup(Alignment.LEADING)
-			.addGroup(groupLayout.createSequentialGroup()
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-					.addGroup(groupLayout.createSequentialGroup()
-						.addGap(22)
-						.addComponent(btnCombinarArchivos, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE))
-					.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-						.addContainerGap()
-						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE))
-					.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-						.addContainerGap()
-						.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 220, Short.MAX_VALUE)))
-				.addContainerGap())
-	);
-	groupLayout.setVerticalGroup(
-		groupLayout.createParallelGroup(Alignment.LEADING)
-			.addGroup(groupLayout.createSequentialGroup()
-				.addContainerGap()
-				.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
-				.addGap(18)
-				.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
-				.addGap(29)
-				.addComponent(btnCombinarArchivos, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
-				.addContainerGap(134, Short.MAX_VALUE))
-	);
+	
+
+	
 	getContentPane().setLayout(groupLayout);
-	btnCombinarArchivos.addMouseListener(new MouseAdapter() 
+	btnCombinarArchivos.addActionListener(new ActionListener()
 	{	@Override
-		public void mouseClicked(MouseEvent e) 
+		public void actionPerformed(ActionEvent e) 
 		{
-			if (dirsSelected(fcORG.getSelectedFile(), fcDEST.getSelectedFile())) 
+			if (auxf.dirsSelected(fcORG.getSelectedFile(), fcDEST.getSelectedFile())) 
 			{
 				File dir = new File(fcORG.getSelectedFile().getAbsolutePath().toString());
 				File[] directoryListing = dir.listFiles();
-				if(existSuppFile(directoryListing))
-				{	
-					PrintStream prinFile = null;
-					try 
-					{
-						prinFile = new PrintStream(fcDEST.getSelectedFile().getAbsolutePath().toString() + File.separator + "combinedResults.txt");
-					}	 
-					catch (FileNotFoundException e1) 
-					{
-						e1.printStackTrace();
-					}					
-					for (int i = 0; i < directoryListing.length; i++) 
-					{
-						String file = directoryListing[i].getPath(), q1 = "";
-						if (supportedFile(file))
-						{	
-							Scanner scanFile = null;
-							try 
-							{
-								scanFile = new Scanner(new File(file));
-							} 
-							catch (FileNotFoundException e1) 
-							{
-								e1.printStackTrace();
-							}
-							while (scanFile.hasNextLine())
-							{
-								if (q1.startsWith("Answer.1"))
-								{
-									prinFile.println(q1);
-									break;
-								}
-								q1 = scanFile.nextLine();
-							}
-							while(scanFile.hasNextLine())
-							{	
-								prinFile.println(scanFile.nextLine()); 
-							}
-							prinFile.println(""); 
-							scanFile.close();
-					 }
-				   }
-					  showMessage("El proceso ha finalizado exitosamente.");
-					  prinFile.close(); 		
-				  }
-				  else
-				  {
-					  showMessage("El directorio especificado está vacío o no contiene\narchivos de tipo soportado (.dat y/o .cd.res.).");
-				  }		 
-			} 
+				String destDir = fcDEST.getSelectedFile().getAbsolutePath().toString();
+				auxf.proceedIfSupportedFilesInDir(directoryListing, destDir);
+			}
 			else
 			{
-				showMessage("Es necesario especificar los directorios antes de comenzar.");
+				auxf.showMessage("Es necesario especificar los directorios antes de comenzar.");
 			}
 		 }
 	 });
+	
+	groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(22)
+							.addComponent(btnCombinarArchivos, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE))
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 220, Short.MAX_VALUE)))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+					.addGap(29)
+					.addComponent(btnCombinarArchivos, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(134, Short.MAX_VALUE))
+		);
 }
 } 
